491e075cf80f08f66e1a08341b100168c2417999
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2022-12-17 23:24:49 +0530

    build complete

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const currYear = new Date().getFullYear;
+  return (
+    <footer>
+      <p>Made by Shubham✨</p>
+    </footer>
+  );
+}
+
+export default Footer;
